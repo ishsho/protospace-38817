fef929d8bae370c10c7e7f2aebd913a587a9174f
ishsho <ishsho1203@gmail.com>
2023-01-12 17:00:12 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, only: [:edit, :update, :show, :destroy]
+  before_action :move_to_edit,only: [:edit]
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -20,11 +21,9 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
@@ -33,13 +32,11 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
     prototype.destroy
     redirect_to root_path
   end
@@ -51,8 +48,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_edit
+    unless  current_user == @prototype.user
       redirect_to action: :index
     end
   end
